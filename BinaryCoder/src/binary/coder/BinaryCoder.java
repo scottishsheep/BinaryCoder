857fb429ed0e8762c017d96e99b3623fdbf2815b
ge28dih <benjamin.schmitz@tum.de>
2021-02-06 19:09:20 +0100

    Template

---
+++ BinaryCoder/src/binary/coder/BinaryCoder.java
@@ -0,0 +1,17 @@
+package binary.coder;
+
+import static binary.coder.helper.Helper.*;
+
+public class BinaryCoder {
+
+	public static void main(String[] args) {
+		String input = getInput();
+		map(input);
+	}
+
+	private static int map(String input) {
+		// TODO: Implementation
+		return -1;
+	}
+
+}
---
+++ BinaryCoder/src/binary/coder/helper/Helper.java
@@ -0,0 +1,12 @@
+package binary.coder.helper;
+
+import java.util.Scanner;
+
+public class Helper {
+	public static String getInput() {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		sc.close();
+		return input;
+	}
+}
