d76defe563f7efad8932d1d04f0d873e99a2f53f
scottishsheep <ina_schmitz@freenet.de>
2021-05-09 10:23:02 +0200

    now also working for negative numbers

--- BinaryCoder/src/binary/coder/BinaryCoder.java
+++ BinaryCoder/src/binary/coder/BinaryCoder.java
@@ -7,53 +7,86 @@ public class BinaryCoder {
 	private static char[] inputchararray;
 
 	public static void main(String[] args) {
-		startprogram();
+		while (true) {
+			try {
+				startprogram();
+			} catch (IllegalArgumentException e) {
+				System.err.println(e.getMessage());
+			}
+			System.out.println("Nochmal?");
+			String in = getInput().toLowerCase();
+			if (!in.equals("yes") && !in.equals("y") && !in.equals("ja") && !in.equals("j")) {
+				break;
+			}
+		}
 
 	}
 
 	private static void startprogram() {
 		System.out.println("Bitte gib einen 32-stelligen Binärcode an!");
+		System.out.println("                               v");
 		String eingabe = getInput();
-		check(eingabe);
-
+		System.out.println("Deine Zahl ist: " + map(eingabe));
 	}
 
 	public static int map(String input) {
-		// TODO: Implementation
+		if (input.length() > 32) {
+			throw new IllegalArgumentException("Keine Zahlen länger als 32 Zeichen!");
+		}
+		while (input.length() < 32) {
+			input = "0" + input;
+		}
 		inputchararray = input.toCharArray();
+
+		for (char c : inputchararray) {
+			if (c != '0' && c != '1') {
+				throw new IllegalArgumentException("Nur die Ziffern '0' und '1' sind erlaubt!");
+			}
+		}
+
 		int[] inputarray = new int[32];
 		for (int i = 0; i < 32; i++) {
 			inputarray[i] = Integer.parseInt(String.valueOf(inputchararray[i]));
 		}
 		int factor;
-		if (inputarray[0] == 0)
+		if (inputarray[0] == 0) {
 			factor = 1;
-		else
+		} else {
+			// negative Zahl
+			// eins abziehen
+			int erste1 = 0;
+			boolean broken = false;
+			LOOP: for (int i = inputarray.length - 1; i > 0; i--) {
+				if (inputarray[i] == 1) {
+					erste1 = i;
+					broken = true;
+					break LOOP;
+				}
+			}
+			if (!broken) {
+				return Integer.MIN_VALUE;
+			}
+			for (int j = erste1; j < inputarray.length; j++) {
+				if (inputarray[j] == 1) {
+					inputarray[j] = 0;
+				} else {
+					inputarray[j] = 1;
+				}
+			}
+
+			// umdrehen
+			for (int counter = 0; counter < inputarray.length; counter++) {
+				inputarray[counter] = inputarray[counter] == 1 ? 0 : 1;
+			}
 			factor = -1;
-		int mynumber=0;
-		for (int j=31; j>0; j--)
-		{if (inputarray[j]==1)
-			mynumber=(int) (mynumber+Math.pow(2, 31-j));}
-		
-		return factor*mynumber;
-	}
 
-	private static void check(String eingabe) {
-		if (eingabe.length() != 32) {
-			System.out.println(eingabe.length());
-			startprogram();
-			return;
-		} else
-
-		{
-			map(eingabe);
-			System.out.println("Deine Zahl ist: " + map(eingabe));
-			System.out.println("Nochmal? (J macht weiter)");
-			String eingabeneu = getInput();
-			if (eingabeneu.equals("J"))
-				startprogram();
-			else return;
 		}
-	}
+		int mynumber = 0;
+		for (int j = 31; j > 0; j--) {
+			if (inputarray[j] == 1)
+				mynumber = (int) (mynumber + Math.pow(2, 31 - j));
+		}
 
+		return factor * mynumber;
+	}
 }
